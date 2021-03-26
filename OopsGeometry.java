6e28ce1fede953911d847296d38d0c8acaae1510
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-26 16:33:08 +0530

    Added:Used compareTo to find which line is greater or lesser or both equal

--- OopsGeometry.java
+++ OopsGeometry.java
@@ -1,37 +1,44 @@
 ///"Welcome to the line computation program "
+
 import java.util.Scanner;
 
 public class OopsGeometry
 {
+
 
 	//method to take user input
 	static double userInput1(String x)
 	{
 		Scanner sc=new Scanner(System.in);
 		System.out.println(x);
+
 		double input=sc.nextDouble();
 		return input;
 	}
 
+
 		//method to calculate length of line
 		 static double calLength(double x1, double y1, double x2, double y2)
 		{
+
 			double LengthOfLine=(int)Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 			return LengthOfLine;
+
 		}
 
 
 				//main method
 				public static void main(String args[])
 				{
+
 					double x1=userInput1("Enter the x1 co-ordinate: ");
 					double y1=userInput1("Enter the y1 co-ordinate: ");
 					double x2=userInput1("Enter the x2 co-ordinate: ");
-			      double y2=userInput1("Enter the y2 co-ordinate: ");
-		       	double p1=userInput1("Enter the p1 co-ordinate: ");
-               double q1=userInput1("Enter the q1 co-ordinate: ");
-               double p2=userInput1("Enter the p2 co-ordinate: ");
-               double q2=userInput1("Enter the q2 co-ordinate: ");
+			      		double y2=userInput1("Enter the y2 co-ordinate: ");
+		       					double p1=userInput1("Enter the p1 co-ordinate: ");
+               				double q1=userInput1("Enter the q1 co-ordinate: ");
+               				double p2=userInput1("Enter the p2 co-ordinate: ");
+               				double q2=userInput1("Enter the q2 co-ordinate: ");
 
 						//passing the values by calling method and assigning to variables
 						int len1=(int)calLength(x1, y1, x2, y2);
@@ -48,9 +55,12 @@ public class OopsGeometry
 						System.out.println("equality of 2 lengths is: "+L1.equals(L2));
 						System.out.println("Comparing 2 lengths : "+Com);
 
+						//comparing which line length is greater than other or equal
 						if(Com<0)
 						{
+
 								System.out.println("Line 1 Length is less than Line 2 Length");
+
 						}
 						else if(Com>0)
 							{
