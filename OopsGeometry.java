bda93745e82c97dff310edd3aa23e4ccb401546f
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-26 16:34:46 +0530

    Added:Used compareTo to find which line is greater or lesser or both equal

--- OopsGeometry.java
+++ OopsGeometry.java
@@ -35,7 +35,8 @@ public class OopsGeometry
 					double y1=userInput1("Enter the y1 co-ordinate: ");
 					double x2=userInput1("Enter the x2 co-ordinate: ");
 			      		double y2=userInput1("Enter the y2 co-ordinate: ");
-		  					double p1=userInput1("Enter the p1 co-ordinate: ");
+
+				  					double p1=userInput1("Enter the p1 co-ordinate: ");
                				double q1=userInput1("Enter the q1 co-ordinate: ");
                				double p2=userInput1("Enter the p2 co-ordinate: ");
                				double q2=userInput1("Enter the q2 co-ordinate: ");
